a737667c919303c0bf99420e5d294f3cd32f88fa
zhoubiao <biao@zhoubiaodeMacBook-Pro.local>
2020-05-04 23:51:18 +0800

    首页底部组件

---
+++ docs/.vuepress/components/footer/footer.vue
@@ -0,0 +1,31 @@
+<template>
+        <footer class="diyFooter">© 2020 ityoudream.cn 版权所有 <span class="target"  @click="handleOpen">{{ code }}</span></footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: 'http://www.beian.miit.gov.cn/',
+      code: '蜀ICP备19011356号'
+    }
+  },
+  methods: {
+    handleOpen () {
+      window.open(this.url)
+    }
+  },
+}
+</script>
+
+<style scoped>
+.diyFooter {
+    padding: 2.5rem;
+    border-top: 1px solid #eaecef;
+    text-align: center;
+    color: lighten(#4e6e8e, 25%);
+}
+.target {
+  cursor: pointer;
+}
+</style>

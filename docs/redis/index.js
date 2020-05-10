b7aea743bf2f4edf1a07709c7dd924b4e2a321ac
zhoubiao <biao@zhoubiaodeMacBook-Pro.local>
2020-05-10 16:29:39 +0800

    配置文件

--- docs/redis/index.js
+++ docs/redis/index.js
@@ -7,7 +7,10 @@ module.exports = () => {
         children: [
           'redis-string/01-aboutRedis.md',
           'redis-string/02-redis-string入门.md',
-          'redis-string/03-redis-string-mybatis.md'
+          'redis-string/03-redis-string-mybatis.md',
+          'redis-string/04-redis-string-serializable.md',
+          'redis-string/05-redis-mybatis-cache.md',
+          'redis-string/06-redis-string-pv.md'
         ]
       }
     ]

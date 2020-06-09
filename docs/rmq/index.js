e2656cf707106ca57a063c056ec8419985993006
zhoubiao188 <zz863146675bb@icloud.com>
2020-06-09 23:18:52 +0800

    配置文件

--- docs/rmq/index.js
+++ docs/rmq/index.js
@@ -7,7 +7,8 @@ module.exports = () => {
         children: [
             'rmq-what.md',
             'rmq-01-install.md',
-            'rmq-02-install.md'
+            'rmq-02-install.md',
+            'rmq-03-quick-start.md'
         ]
       }
     ]

17bf51931c90fe83c4a632efb0a1f582715833fa
zhoubiao188 <zz863146675bb@icloud.com>
2020-06-12 21:17:04 +0800

    rabbitmq 配置文件

--- docs/rmq/index.js
+++ docs/rmq/index.js
@@ -10,7 +10,10 @@ module.exports = () => {
             'rmq-02-install.md',
             'rmq-03-quick-start.md',
             'rmq-04-connection.md',
-            'rmq-05-direct.md'
+            'rmq-05-direct.md',
+            'rmq-06-topic.md',
+            'rmq-07.fanout.md',
+            'rmq-08-durable.md'
         ]
       }
     ]

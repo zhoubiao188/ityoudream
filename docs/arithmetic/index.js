6ecd5e193f13944c346247be45a71164f4a770e4
zhoubiao188 <zz863146675bb@icloud.com>
2020-06-15 22:48:55 +0800

    配置文件

--- docs/arithmetic/index.js
+++ docs/arithmetic/index.js
@@ -5,7 +5,9 @@ module.exports = () => {
         title: "数据结构与算法",
         collapsable: true,
         children: [
-            'arith-01.md'
+            'arith-01.md',
+            'arith-02.md',
+            'arith-03.md'
         ]
       }
     ]

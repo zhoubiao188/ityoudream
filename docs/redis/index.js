92d2120ecffd846211c838cc3cfb31d20d03a02e
zhoubiao188 <zz863146675bb@icloud.com>
2020-08-25 23:29:10 +0800

    update config

--- docs/redis/index.js
+++ docs/redis/index.js
@@ -65,7 +65,8 @@ module.exports = () => {
             'redis-list/docker-redis-fbs.md',
             'redis-list/redis-lock-about.md',
             'redis-list/redis-lock-waitTime.md',
-            'redis-list/redis-setnx.md'
+            'redis-list/redis-setnx.md',
+            'redis-list/redis-lock-lua.md'
           ]
           }
     ]

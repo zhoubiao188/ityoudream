7979915965a98b699949eb5d03606da7bd256d4a
zhoubiao188 <zz863146675bb@icloud.com>
2020-11-08 22:01:26 +0800

    modify index.js

---
+++ docs/java8/index.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+    return [
+      '',
+      {
+        title: "Java8编程",
+        collapsable: true,
+        children: [
+            '/lambda/Thread.md',
+            '/lambda/SampleFormat.md',
+            '/lambda/Min.md',
+            '/lambda/Lazy.md',
+            '/lambda/FunctionInterface.md',
+            '/lambda/Function2.md',
+            '/lambda/Function',
+            '/lambda/Curry.md',
+            '/lambda/Core.md'
+        ]
+      }
+    ]
+  }

04be5cbda17b4a38e1f43d94baf7a44c023068e5
zhoubiao188 <zz863146675bb@icloud.com>
2021-02-12 01:11:48 +0800

    多线程的三种创建方式

--- docs/process/index.js
+++ docs/process/index.js
@@ -5,7 +5,8 @@ module.exports = () => {
         title: "Java线程",
         collapsable: true,
         children: [
-          '01-about-process.md'
+          '01-about-process.md',
+          '02-quickstart-thread.md'
         ]
       }
     ]

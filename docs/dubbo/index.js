91c66674d2ea0ce5ae1ba49a9dc0b3a3db920b4b
zhoubiao188 <zz863146675bb@icloud.com>
2021-02-12 20:49:30 +0800

    recover dubbo

--- docs/dubbo/index.js
+++ docs/dubbo/index.js
@@ -5,12 +5,12 @@ module.exports = () => {
         title: "Dubbo从入门到精通",
         collapsable: true,
         children: [
-          'dubbo1.md',
-          'dubbo2.md',
-          'dubbo3.md',
-          'dubbo4.md',
-          'dubbo5.md',
-          'dubbo6.md'
+          // 'dubbo1.md',
+          // 'dubbo2.md',
+          // 'dubbo3.md',
+          // 'dubbo4.md',
+          // 'dubbo5.md',
+          // 'dubbo6.md'
         ]
       }
     ]
